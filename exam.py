7518f7f3b473681db460e4e4b44169e7919b3305
Chris Frishkorn <frishkorn@gmail.com>
2020-04-28 16:09:24 -0400

    Added ask user for additional questions.

--- exam.py
+++ exam.py
@@ -2,13 +2,13 @@
 
 # exam.py
 # C. Frishkorn 04/28/2020
-# version: 0.0.2
+# version: 0.0.3
 # ------------------------
 import json
 
 # Ask user if they want to add questions / answers.
 selection = input("Would you like to enter new questions? (Y/N): ")
-if selection == "Y":
+while selection == "Y":
     # Ask user to input the question.
     question = input("Question: ")
 
@@ -29,12 +29,9 @@ if selection == "Y":
 
         print(json.dumps(data, sort_keys=True, indent=4))
 
-else:
-    print("Saw N")
-
-
-# Ask user if they would like to enter another question / answer.
-
+        # Ask user if they would like to enter another question / answer.
+        selection = input("Would you like to add another question? (Y/N): ")
+ 
 # Load questions file.
 
 # Pick a random question and ask user to answer it.
