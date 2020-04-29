2c13f14452c432e5dbf0ff0d8e9eec6ab3d941d6
Chris Frishkorn <frishkorn@gmail.com>
2020-04-29 11:18:16 -0400

    Added random function, made input case-insensitive.

--- exam.py
+++ exam.py
@@ -2,20 +2,26 @@
 
 # exam.py
 # C. Frishkorn 04/28/2020
-# version: 0.0.8
+# version: 0.0.9
 # ------------------------
 import json
+from random import randint
 
 # JSON write function (pretty print).
 def write_json(data, filename='data.json'):
     with open(filename,'w') as f:
         json.dump(data, f, indent=4)
 
+# Get random function.
+def get_random(length):
+    value = randint(1, length)
+    return value
+
 # Ask user if they want to add questions / answers.
 print("2019-2023 General Class Pool")
 print("2nd & Final Public Release with Errata - March 15, 2019")
-selection = input("Would you like to enter new questions? (Y/N): ")
-while selection == "Y":
+selection = input("Would you like to enter new questions? (Y/N): ").lower()
+while selection == "y":
     # Ask user to input the question.
     question = input("Question: ")
 
@@ -26,7 +32,7 @@ while selection == "Y":
         answerList.append(answer)
 
     # Ask user which one is the right answer.
-    correct = input("Which answer is correct?: ")
+    correct = input("Which answer is correct?: ").upper()
     
     # Save Q&A to a JSON formatted file.
     question_data = {'question':question,'answers':[{'A':answerList[0],'B':answerList[1],'C':answerList[2],'D':answerList[3]}],'right_answer':correct}
@@ -40,7 +46,7 @@ while selection == "Y":
     write_json(file_data)
 
     # Ask user if they would like to enter another question / answer.
-    selection = input("Would you like to add another question? (Y/N): ")
+    selection = input("Would you like to add another question? (Y/N): ").lower()
     
 # Load questions file.
 with open('data.json', 'r') as f:
@@ -55,11 +61,12 @@ print("There are %d total questions in the pool." % (pool_len))
     # print(pool['question_pool'][1])
 
 # Ask user if they would like to review or take practice exam.
-selection = input("Would you like to practice or review? (P/R): ")
-print(selection)
-
-
-
+selection = input("Would you like to practice or review? (P/R): ").lower()
+if selection == "r":
+     index = get_random(pool_len)
+     print(pool['question_pool'][index - 1])
+else:
+    print("Found P")
 
 
 
