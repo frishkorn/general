a4e1fb3fc68dbbae0a3f1d331dc7a8a9abfd41f7
Chris Frishkorn <frishkorn@gmail.com>
2020-12-07 14:05:54 -0500

    Fixed JSON for answers object.

--- exam.py
+++ exam.py
@@ -154,7 +154,7 @@ def add_question(selection):
         correct = input("Which answer is correct?: ").upper()
 
         # Save Q&A to a JSON formatted file.
-        question_data = {'question_id':question_id,'question_group':question_group,'question':question,'answers':[{"A":answer_list[0], "B":answer_list[1], "C":answer_list[2], "D":answer_list[3]}],'right_answer':correct,'cr_attempts':0,'in_attempts':0}
+        question_data = {'question_id':question_id,'question_group':question_group,'question':question,'answers':{"A":answer_list[0], "B":answer_list[1], "C":answer_list[2], "D":answer_list[3]},'right_answer':correct,'cr_attempts':0,'in_attempts':0}
 
         # Append new questions to data.json.
         with open('data.json') as json_file:
